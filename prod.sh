70a46ea8f54117083edeec414ad0155f1bf795b5
Michael Gruener <michael.gruener@bedag.ch>
2017-10-21 13:09:19 +0200

    Restart instead of stop/start

--- prod.sh
+++ prod.sh
@@ -3,5 +3,4 @@
 cp "$(dirname ${0})/git-watcher.service" /etc/systemd/system/git-watcher.service
 systemctl daemon-reload
 systemctl enable git-watcher.service
-systemctl stop git-watcher.service
-systemctl start git-watcher.service
+systemctl restart git-watcher.service
